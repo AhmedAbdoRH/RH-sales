f782f52e422b8e9bbc929c20328cb39b79d28a04
Rehlat E-Commerce <rehlathadaf@gmail.com>
2025-10-26 09:47:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "domainview",
+  "appId": "1:226934922867:web:db492564c92b9b95f79406",
+  "apiKey": "AIzaSyA-AuKkPZiuQdA-NIPjObheWabwnrqwG7g",
+  "authDomain": "domainview.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "226934922867"
+};
